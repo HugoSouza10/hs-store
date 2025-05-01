08fa01ae02a547dd737c3d7490aaa07eee4bc85b
Hugo Souza <hugo.html.com@gmail.com>
2025-05-01 00:08:50 -0300

    feat(core): adicionar função CalculatePricing para cálculo dinâmico de preços

---
+++ src/app/helpers/calculatePricing.ts
@@ -0,0 +1,16 @@
+import { Product } from "@/types/product";
+
+interface ProductWithDiscount extends Product {
+    productWithDiscount: number;
+}
+
+export const calculateDiscountedPrice = (product: Product): ProductWithDiscount => {
+    const discountAmount =  (product.basePrice * product.discountPercentage) / 100;
+
+    const discountedPrice = product.basePrice - discountAmount;
+
+    return {
+        ...product,
+        productWithDiscount: Number(discountedPrice.toFixed(2)) // Arredonda para 2 casas decimais
+    }
+};
