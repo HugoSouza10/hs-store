2d93e1dc8be982871e663356611348bcb8a0733b
Hugo Souza <hugo.html.com@gmail.com>
2025-07-11 16:40:14 -0300

    feat(component): adicionar fallback 'Nenhum produto encontrado' para lista de produtos vazia

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -7,12 +7,15 @@ interface ProductListProps {
 }
 
 export const ProductList = ({ products }: ProductListProps) => {
-    console.log(products)
     return (
         <div className="flex w-full mt-5 mb-7 gap-4 overflow-x-auto  [&::-webkit-scrollbar]:hidden">
-          {products.map((product: any, key=product.id)=>
-               <ProductItem products={calculateDiscountedPrice(product)} key={key}/>
-            )}
+          {products.length > 0 ? (
+            products.map((product)=>
+               <ProductItem product={calculateDiscountedPrice(product)} key={product.id}/>
+            )
+          ) : (
+            <p>Nenhum produto encontrado</p>
+          )}
         </div>
     )
 }
