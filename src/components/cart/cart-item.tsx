442f85c793e780986abb5e499f49448e376a9922
Hugo Souza <hugo.html.com@gmail.com>
2025-07-11 16:43:27 -0300

    feat(cart-item): chamar QuantitySelector e implementar função para remover produto do carrinho

--- src/components/cart/cart-item.tsx
+++ src/components/cart/cart-item.tsx
@@ -1,8 +1,17 @@
 import { Product } from "@/types/product";
+import { useCart } from "@/context/CartContext";
 import { Trash } from "lucide-react";
 import Image from 'next/image';
+import { ProductWithDiscount } from "@/app/helpers/calculatePricing";
+import { QuantitySelector } from "@/components/ui/QuantitySelector";
 
 export default function CartItem({product}: any) {
+    const { removeProduct } = useCart();
+
+    const handleremoveProductFromCartClick = () => {
+      removeProduct(product.id);
+    }
+
     console.log(product);
     return (
       <>
@@ -22,15 +31,16 @@ export default function CartItem({product}: any) {
              <div className="mt-4">
               <div> {/* Container com largura máxima */}
                 <span className="truncate block text-xs"> {/* "block" para ocupar a largura total */}
-                  {product.description}
+                  {product.name}
                 </span>
               </div>
               <div>
                 <span className="font-bold text-sm">R$: {product.productWithDiscount}</span>
                 <span className="text-xs ml-1 line-through text-[#676767]">R$: {product.basePrice}</span>
               </div>
+              <QuantitySelector id={product.id} quantity={product.quantity}/>
             </div>
-            <div className="flex items-center justify-center w-8 h-8 border border-[#1A1A1A] rounded-sm p-2">
+            <div onClick={handleremoveProductFromCartClick} className="flex items-center justify-center w-8 h-8 border border-[#1A1A1A] rounded-sm p-2">
               <Trash size={16}/>
             </div>
         </div>
