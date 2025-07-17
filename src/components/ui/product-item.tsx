79d2f5745182f94eaa3507eeb6f05843dadb710f
Hugo Souza <hugo.html.com@gmail.com>
2025-07-17 18:01:20 -0300

    feat(ProductItem): integrar CartContext com handleAddProduct

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,31 +1,41 @@
+"use client"
 import Image from "next/image"
 import { Badge } from "./badge"
 import Link from "next/link"
-import { Product } from "@/types/product";
+import { CartProduct, useCart } from "@/context/CartContext";
+import { ProductWithDiscount } from "@/app/helpers/calculatePricing";
 
 interface ProductListProps {
-    products: Product;
+    product: ProductWithDiscount;
 }
 
+export const ProductItem = ({product}: ProductListProps) => {
+   const { addProduct } = useCart();
 
-export const ProductItem = ({products}: ProductListProps) => {
+  const handleAddProduct = () => {
+        const cartProduct: CartProduct = {
+            ...product,
+            quantity: 1 // Valor padrão ao adicionar ao carrinho
+        };
+        addProduct(cartProduct);
+   }
    return (
    <>
     {/*Area da foto */}
-    <Link href={`product/${products.slug}`}>
+    <Link href={`product/${product.slug}`}>
       <div className="flex-col gap-4 px-0">
       
          <div className="relative flex flex-col items-center justify-center rounded-sm w-[156px] h-[170px] bg-accent">
             {/* Tag de desconto */}
             <div className="absolute p-2 top-0 left-0 font-bold">
-            {products.discountPercentage > 0 &&(
-                  <Badge className="rounded-full">
-                     {products.discountPercentage}%
+            {product.discountPercentage > 0 &&(
+                  <Badge onClick={handleAddProduct} className="rounded-full">
+                     {product.discountPercentage}%
                   </Badge> 
             )}
             </div>
             <Image
-                  src={products.imageUrls[0]}
+                  src={product.imageUrls[0]}
                   width={0}
                   height={0}
                   sizes="100vw"
@@ -33,18 +43,18 @@ export const ProductItem = ({products}: ProductListProps) => {
                   style={{
                      objectFit:'contain'
                   }}
-                  alt={products.slug}
+                  alt={product.slug}
                />
          </div>
          <div className="mt-4">
             <div className="max-w-[150px]"> {/* Container com largura máxima */}
                <span className="truncate block"> {/* "block" para ocupar a largura total */}
-                  {products.name}
+                  {product.name}
                </span>
             </div>
             <div>
-               <span className="font-bold">R$: {products.productWithDiscount}</span>
-               <span className="text-xs ml-1 line-through text-[#676767]">R$: {products.basePrice}</span>
+               <span className="font-bold">R$: {product.productWithDiscount}</span>
+               <span className="text-xs ml-1 line-through text-[#676767]">R$: {product.basePrice}</span>
             </div>
          </div>
       </div>
