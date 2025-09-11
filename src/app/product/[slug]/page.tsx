522ecbe56565b74218bbb7363113820a61c557f7
Hugo Souza <hugo.html.com@gmail.com>
2025-09-10 22:20:09 -0300

    feat: implementar carrosel de relacionados na página de produto

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,13 +1,13 @@
-import { QuantitySelector } from "@/components/ui/QuantitySelector";
+
 import ProductGallery from "../components/ProductGallery";
 import { Product } from "@/types/product";
-import { CartProvider } from '@/context/CartContext';
 import { calculateDiscountedPrice } from "@/app/helpers/calculatePricing";
-import { Badge } from "@/components/ui/badge";
 import { AddToCartButton } from "../components/AddToCartButton";
 import { ProductInfo } from "../components/ProductInfo";
 import { ProductPrice } from "../components/ProductPrice";
 import { ShippingInfo } from "../components/ShippingInfo";
+import { SectionTitle } from "@/components/ui/section-title";
+import { ProductList } from "@/components/ui/product-list";
 
 // Fetch no servidor (SSR)
 const getProduct = async (slug: string): Promise<Product> => {
@@ -16,11 +16,20 @@ const getProduct = async (slug: string): Promise<Product> => {
   return res.json();
 };
 
+async function fetchRelated() {
+  const response = await fetch('http://localhost:3000/api/product');
+  if (!response.ok) {
+    throw new Error('Falha ao buscar produtos');
+  }
+  const data = await response.json();
+  // Filtra os produtos com desconto
+  return data.filter((product: {discountPercentage: number}) => product.discountPercentage > 0)
+}
+
 const ProductDetail = async ({ params }: { params: { slug: string } }) => {
   const data = await getProduct(params.slug);
   const product = calculateDiscountedPrice(data);
-  console.log(product)
-
+  const relatedProducts = await fetchRelated();
   return (
          <div>
             <ProductGallery
@@ -38,6 +47,8 @@ const ProductDetail = async ({ params }: { params: { slug: string } }) => {
                {/* Botão Adicionar ao Carrinho */}
               <AddToCartButton product={product}/>
               <ShippingInfo/>
+              <SectionTitle className="mt-14 font-bold text-base">PRODUTOS RECOMENDADOS</SectionTitle>
+              <ProductList products={relatedProducts}/>
             </article>
         </div>
   );
