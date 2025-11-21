e83298ca3ff0b826d6d162e877203956ea68ac22
Hugo Souza <hugo.html.com@gmail.com>
2025-11-21 15:48:39 -0300

    feat: Adicionar cachê RSC na função getCategoriesAll

--- src/app/_data_access/categoryData.ts
+++ src/app/_data_access/categoryData.ts
@@ -1,8 +1,19 @@
+import { unstable_cache } from 'next/cache';
 import { fetchData } from '../_data_access/fetchData';
 const API_BASE_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
 
 //Pega todas as categorias dos produtos
-export async function getCategoriesAll() {
-  const url = `${API_BASE_URL}/api/category`;
-  return await fetchData(url);
-}
+
+// cache de Server Component do Next.js (RSC Cache)
+export const getCategoriesAll = unstable_cache(
+  async () => {
+    console.log('🔄 Cache MISS - buscando categorias da API');
+    const url = `${API_BASE_URL}/api/category`;
+    return await fetchData(url);
+  },
+  ['categories-all'],
+  {
+    tags: ['categories'],
+    revalidate: 600,
+  }
+);
