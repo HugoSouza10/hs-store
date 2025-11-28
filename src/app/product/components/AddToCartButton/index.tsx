29686148b19f612a1d65c2c3ab4905301dea05b5
Hugo Souza <hugo.html.com@gmail.com>
2025-11-27 23:25:06 -0300

    feat: Adicionar tost ao adicionar produto no carrinho

--- src/app/product/components/AddToCartButton/index.tsx
+++ src/app/product/components/AddToCartButton/index.tsx
@@ -1,17 +1,31 @@
 "use client";
 import { useCart } from "@/context/CartContext";
 import { ProductWithDiscount } from "@/app/helpers/calculatePricing";
+import { toast } from "sonner";
 
-export const AddToCartButton = ({ product }: { product: ProductWithDiscount }) => {
-  console.log(product)
+export const AddToCartButton = ({
+  product,
+}: {
+  product: ProductWithDiscount;
+}) => {
+  console.log(product);
   const { addProduct } = useCart();
   // Criar um CartProduct com a quantidade 1, preservando o productWithDiscount existente
-  const productWithQuantity = {
-    ...product,               // Espalha todas as propriedades do produto
-    quantity: 1,               // Define a quantidade como 1
+
+  const handleAddToCart = () => {
+    const productWithQuantity = {
+      ...product, // Espalha todas as propriedades do produto
+      quantity: 1, // Define a quantidade como 1
+    };
+    addProduct(productWithQuantity);
+    toast.success("Produto adicionado com sucesso!");
   };
+  
   return (
-    <button onClick={() => addProduct(productWithQuantity)}  className="w-full py-3 px-6 bg-primary mt-7 font-bold text-center rounded-xl">
+    <button
+      onClick={handleAddToCart}
+      className="w-full py-3 px-6 bg-primary mt-7 font-bold text-center rounded-xl"
+    >
       Adicionar ao Carrinho
     </button>
   );
