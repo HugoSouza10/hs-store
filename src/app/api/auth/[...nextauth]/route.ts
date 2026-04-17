13a2cb2298c9ae743f9a0b6b0d6985d525c537be
Hugo Souza <hugo.html.com@gmail.com>
2026-04-16 23:20:27 -0300

    refactor(auth): refatorar autenticação para NextAuth v5

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,4 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { handlers } from "@/lib/auth"
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ session, token }) {
-        return {
-            ...session,
-            user: {
-              ...session.user!, //Quando a gent usar o ! no final, indica que nunca vai ser underfine
-              id: token.sub!,
-            },
-        };
-    },
-  },
-});
-
-export { handler as GET, handler as POST };
+export const GET = handlers.GET
+export const POST = handlers.POST
