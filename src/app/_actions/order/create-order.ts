dd3ce0cfe0ba4ed1b9a1caf9515d6c268851bc66
Hugo Souza <hugo.html.com@gmail.com>
2026-05-05 22:18:46 -0300

    feat: Criar server action createOrder para criar um pedido

--- src/app/_actions/order/create-order.ts
+++ src/app/_actions/order/create-order.ts
@@ -2,29 +2,30 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@/lib/auth";
+import { createOrderSchema, CreateOrderInput } from "./schemas";
 
 //Responsável por criar o pedido de acordo com usuário
-export async function createOrder(cart: any) {
-    console.log("Criando pedido com os seguintes dados:", cart);
+export async function createOrder(cart: CreateOrderInput) {
   const session = await auth();
 
   if (!session?.user?.id) {
     throw new Error("Usuário não autenticado");
   }
+  const validatedCart = createOrderSchema.parse(cart);
 
   console.log(cart)
   const order = await db.order.create({
     //Data: Sempre vai existir, pois no prisma a gente precisa especificar esses dados através desse objeto
     data: {
       userId: session.user.id,
-      subtotal: cart.subtotal,
-      discount: cart.discount,
-      total: cart.total,
-      status: "PENDING",
+      subtotal: validatedCart.subtotal,
+      discount: validatedCart.discount,
+      total: validatedCart.total,
+      status: "PAGO",
 
       //Aqui vamos criar o OrderItem
       items: {
-        create: cart.items.map((item: any) => ({
+        create: validatedCart.items.map((item) => ({
           productId: item.id,
           quantity: item.quantity,
           price: item.price,
